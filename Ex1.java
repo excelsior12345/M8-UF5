7892bb9f8acdf51c2f1294a58fbb8d4ed443bb83
excelsior12345 <106153892+excelsior12345@users.noreply.github.com>
2022-05-24 13:07:02 +0200

    bb

---
+++ Ex1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package uf6.ex1;
+
+/**
+ *
+ * @author josue
+ */
+public class Ex1 {hola
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
